f959d7a53ed8e0bcec3ab9911f6ee38da79c8a79
gongpei <ggongpei@gmail.com>
2022-07-24 17:30:46 +0800

    feat: glyph bg

--- src/App.tsx
+++ src/App.tsx
@@ -26,14 +26,38 @@ const defaultTab = {
     active: false,
     description: 'Font subset based on Google font unicode range',
     link: ''
+  },
+  glyph: {
+    text: 'Glyph',
+    active: false,
+    description: 'This table contains information that describes the glyphs in the font in the TrueType outline format. Information regarding the rasterizer (scaler) refers to the TrueType rasterizer. ',
+    link: 'https://docs.microsoft.com/en-us/typography/opentype/spec/glyf'
   }
 };
 
+interface IGlyph {
+  advanceWidth: number,
+  contours: Array<Array<{
+    onCurve: boolean
+    x: number
+    y: number
+  }>>,
+  leftSideBearing: number,
+  name: string,
+  xMax: number,
+  xMin: number,
+  yMax: number,
+  yMin: number,
+};
+
 type TTab = keyof typeof defaultTab;
+type TGlyphKey = keyof IGlyph;
 
 const icons = {
   doc: '<svg t="1658586064814" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3065" width="200" height="200"><path d="M664.132267 0H110.933333v1024h802.133334V248.9344L664.132267 0z m10.001066 58.2656L854.801067 238.933333H674.133333V58.2656zM145.066667 989.866667V34.133333h494.933333v238.933334h238.933333v716.8h-733.866666z" fill="" p-id="3066"></path><path d="M281.6 290.133333h153.6a17.066667 17.066667 0 1 0 0-34.133333h-153.6a17.066667 17.066667 0 1 0 0 34.133333zM571.733333 631.466667h-290.133333a17.066667 17.066667 0 1 0 0 34.133333h290.133333a17.066667 17.066667 0 1 0 0-34.133333zM742.4 512h-119.466667a17.066667 17.066667 0 1 0 0 34.133333h119.466667a17.066667 17.066667 0 1 0 0-34.133333zM281.6 426.666667h85.333333a17.066667 17.066667 0 1 0 0-34.133334h-85.333333a17.066667 17.066667 0 1 0 0 34.133334zM537.6 426.666667h119.466667a17.066667 17.066667 0 1 0 0-34.133334h-119.466667a17.066667 17.066667 0 1 0 0 34.133334zM435.2 409.6c0 4.437333 1.877333 8.874667 4.949333 12.117333 3.242667 3.072 7.68 4.949333 12.117334 4.949334 4.437333 0 8.874667-1.877333 12.117333-4.949334 3.072-3.242667 4.949333-7.68 4.949333-12.117333 0-4.4544-1.877333-8.891733-4.949333-12.117333-6.314667-6.314667-17.749333-6.314667-24.234667 0-3.072 3.2256-4.949333 7.662933-4.949333 12.117333zM366.933333 512a17.066667 17.066667 0 1 0 0 34.133333h170.666667a17.066667 17.066667 0 1 0 0-34.133333h-170.666667zM281.6 546.133333c4.437333 0 8.874667-1.877333 12.117333-4.949333 3.072-3.242667 4.949333-7.68 4.949334-12.117333 0-4.4544-1.877333-8.891733-4.949334-12.117334-6.314667-6.314667-17.92-6.314667-24.234666 0-3.072 3.2256-4.949333 7.492267-4.949334 12.117334 0 4.437333 1.877333 8.874667 4.949334 12.117333 3.242667 3.072 7.68 4.949333 12.117333 4.949333zM742.4 750.933333h-119.466667a17.066667 17.066667 0 1 0 0 34.133334h119.466667a17.066667 17.066667 0 1 0 0-34.133334zM537.6 750.933333h-170.666667a17.066667 17.066667 0 1 0 0 34.133334h170.666667a17.066667 17.066667 0 1 0 0-34.133334zM269.482667 755.882667a17.288533 17.288533 0 0 0 0 24.234666c3.242667 3.072 7.68 4.949333 12.117333 4.949334 4.608 0 8.874667-1.877333 12.117333-4.949334 3.072-3.242667 4.949333-7.68 4.949334-12.117333a16.8448 16.8448 0 0 0-4.949334-11.946667c-6.314667-6.485333-17.749333-6.485333-24.234666-0.170666zM730.282667 397.482667c-3.072 3.2256-4.949333 7.662933-4.949334 12.117333 0 4.437333 1.877333 8.874667 4.949334 12.117333 3.242667 3.072 7.68 4.949333 12.117333 4.949334 4.437333 0 8.874667-1.877333 12.117333-4.949334 3.242667-3.242667 4.949333-7.68 4.949334-12.117333 0-4.4544-1.706667-8.891733-4.949334-12.117333-6.485333-6.314667-17.749333-6.314667-24.234666 0zM669.184 660.650667c3.072-3.242667 4.949333-7.68 4.949333-12.117334 0-4.4544-1.877333-8.891733-4.949333-12.117333-6.485333-6.314667-17.92-6.314667-24.234667 0-3.072 3.2256-4.949333 7.662933-4.949333 12.117333 0 4.437333 1.877333 8.874667 4.949333 12.117334 3.242667 3.2256 7.509333 4.949333 12.117334 4.949333s8.874667-1.877333 12.117333-4.949333zM730.282667 636.416c-3.072 3.2256-4.949333 7.662933-4.949334 12.117333 0 4.437333 1.877333 8.874667 4.949334 12.117334 3.242667 3.072 7.509333 4.949333 12.117333 4.949333s8.874667-1.877333 12.117333-4.949333c3.072-3.242667 4.949333-7.68 4.949334-12.117334 0-4.4544-1.877333-8.891733-4.949334-12.117333-6.314667-6.314667-17.92-6.314667-24.234666 0z" fill="" p-id="3067"></path></svg>'
 };
+
+const pixelRatio = window.devicePixelRatio || 1;
 
 function App() {
   // const [file, setFile] = useState<File | null>(null);
@@ -45,6 +69,8 @@ function App() {
 
   const [rangePerPage, setRangePerPage] = useState(10);
   const [rangePage, setRangePage] = useState(0);
+
+  const [curGlyph, setCurGlyph] = useState<IGlyph | null>(null);
 
   const handleChange = async (file: File) => {
     const fontBuffer = await file.arrayBuffer();
@@ -75,7 +101,7 @@ function App() {
     console.log(css, {ttf});
   };
 
-  const { name: fontNameObj, glyf, cmap } = ttf?.data || {};
+  const { name: fontNameObj, glyf, cmap, head, hhea } = ttf?.data || {};
   const os2 = ttf?.data['OS/2'];
 
   const openWindow = (link = '', e: React.MouseEvent<HTMLElement, MouseEvent>) => {
@@ -129,6 +155,45 @@ function App() {
       // }
     })
   }, [cmap, searchCMAP]);
+
+  useEffect(() => {
+    const glyphBgCanvas = document.querySelector('.glyph-canvas-el') as HTMLCanvasElement;
+    if (!glyphBgCanvas) {
+      return;
+    }
+    const margin = 50;
+    const w = glyphBgCanvas.width;
+    const h = glyphBgCanvas.height;
+    const glyphW = w - margin * 2;
+    const glyphH = h - margin * 2;
+    // const head = font.tables.head;
+    const maxHeight = head.yMax - head.yMin;
+    const ctx = glyphBgCanvas.getContext('2d');
+
+    const glyphScale = Math.min(glyphW / (head.xMax - head.xMin), glyphH/maxHeight);
+    const glyphSize = glyphScale * font.unitsPerEm;
+    const glyphBaseline = margin + glyphH * head.yMax / maxHeight;
+
+    function hline(text: string, yunits: number) {
+      const ypx = glyphBaseline - yunits * glyphScale;
+      ctx!.font = '30px sans-serif';
+      ctx!.fillStyle = '#000';
+      ctx!.fillText(text, 2, ypx - 20);
+      ctx!.fillStyle = '#000';
+      ctx!.fillRect(80, ypx, w, 1);
+    }
+
+    ctx!.clearRect(0, 0, w, h);
+    ctx!.fillStyle = '#000';
+
+    hline('Baseline', 0);
+    hline('yMax', head.yMax);
+    hline('yMin', head.yMin);
+    hline('Ascender', hhea.ascender);
+    hline('Descender', hhea.descender);
+    hline('Typo Ascender', os2.sTypoAscender);
+    hline('Typo Descender', os2.sTypoDescender);
+  }, [curGlyph, head, hhea, os2]);
 
   // useEffect(() => {
   //   const css = `
@@ -216,9 +281,13 @@ function App() {
                   <div className="unicode-search">
                     <input type="text" placeholder='Search Code' onInput={onSearchCMAP} />
                   </div>
+                  {/* {JSON.stringify(curGlyph)} */}
                   <ol className='unicode-list'>
                     {cmapListToShow.slice(cmapPage * cmapPerPage, (cmapPage + 1) * cmapPerPage).map(c => (
-                      <li>
+                      <li onClick={() => {
+                        setCurGlyph(glyf[cmap[c]]);
+                        switchTab('glyph');
+                      }}>
                         <div className="unicode-cover">
                           <span>code: {c}</span>
                           <span>glyph: {cmap[c]}</span>
@@ -228,13 +297,6 @@ function App() {
                     ))}
                   </ol>
                   {createPagination(cmapListToShow, cmapPage, cmapPerPage, setCmapPage)}
-                  {/* {cmapListToShow.length > cmapPerPage ? <ol className="pagination">
-                    <li onClick={() => setCmapPage(0)}>First Page</li>
-                    <li onClick={() => cmapPage > 0 && setCmapPage(cmapPage - 1)}>Prev</li>
-                    <li className='curpage'>{cmapPage + 1}/{Math.floor(cmapListToShow.length / cmapPerPage) + 1}</li>
-                    <li onClick={() => cmapPage < Math.floor(cmapListToShow.length / cmapPerPage) && setCmapPage(cmapPage + 1)}>Next</li>
-                    <li onClick={() => setCmapPage(Math.floor(cmapListToShow.length / cmapPerPage))}>Last Page</li>
-                  </ol> : ''} */}
                 </>
               ): ''}
 
@@ -274,6 +336,19 @@ function App() {
                   </ol>
                   {createPagination(subsetRangeToShow, rangePage, rangePerPage, setRangePage)}
                 </>
+              ): ''}
+
+              {tab.glyph.active && curGlyph ? (
+                <div className='glyph-info'>
+                    <ul className="glyph-sidebar">
+                      {Object.keys(curGlyph).filter(k => k !== 'contours').map(k => {
+                        return <li><span className='glyph-sidebar-title'>{k}:</span>{curGlyph[k as TGlyphKey]}</li>
+                      })}
+                    </ul>
+                    <div className="glyph-canvas">
+                      <canvas height={1000} width={1000} className="glyph-canvas-el"></canvas>
+                    </div>
+                </div>
               ): ''}
             </div>
           </div>
